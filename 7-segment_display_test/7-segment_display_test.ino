38fc9a2dc8f1d05585aec73a32472504045d355f
stoniejimmy <stoniejimmy@users.noreply.github.com>
2018-08-08 09:31:27 +0800

    修正&新增註解

--- 7-segment_display_test/7-segment_display_test.ino
+++ 7-segment_display_test/7-segment_display_test.ino
@@ -30,38 +30,46 @@
 //引用Wire資料庫來使用i2c與主控制器連線
 #include <Wire.h>
 
-int num = 0; // count value
-
+//定義七段顯示器IC的腳位
 int seg_rst = 13;
 int seg_a = A0;
 int seg_b = A3;
 int seg_c = A1;
 int seg_d = A2;
+//註: abcd四腳為因digital腳位已用盡，故使用analog腳位來做digital輸出
 
+//定義右上狀態指示RGB LED的腳位
 int ur = 7;
 int ug = 8;
 int ub = 9;
+
+//定義右上狀態指示RGB LED的腳位
 int lr = 10;
 int lg = 11;
 int lb = 12;
 
+//定義要從master接收的燈號訊號
 volatile int val1_a; volatile int val1_b; volatile int val1;
 volatile int val2_a; volatile int val2_b; volatile int val2;
 volatile int val3_a; volatile int val3_b; volatile int val3;
 
-int seg_state = 0;
-int led_state_1 = 0;
-int led_state_2 = 0;
+//顯示數字及燈號的變數設定
+int seg_state = 0; //7段顯示器
+int led_state_1 = 0; //右上方系統指示燈
+int led_state_2 = 0; //右下方馬達狀態指示燈
 
 void setup() {
+  //開啟序列埠以做監控
   Serial.begin(9600);
+  //準備Wire以做i2c傳送及接收
   Wire.begin();
 
-  pinMode(seg_a, OUTPUT); //黃色線
-  pinMode(seg_b, OUTPUT); //紫色線
-  pinMode(seg_c, OUTPUT); //白色線
-  pinMode(seg_d, OUTPUT); //綠色線
-  pinMode(seg_rst, OUTPUT); // //橘色線
+  //將各digital腳位設定為輸出
+  pinMode(seg_a, OUTPUT);
+  pinMode(seg_b, OUTPUT);
+  pinMode(seg_c, OUTPUT);
+  pinMode(seg_d, OUTPUT);
+  pinMode(seg_rst, OUTPUT);
   pinMode(ur, OUTPUT);
   pinMode(ug, OUTPUT);
   pinMode(ub, OUTPUT);
@@ -69,10 +77,12 @@ void setup() {
   pinMode(lg, OUTPUT);
   pinMode(lb, OUTPUT);
 
+  //測試所有燈號
   digitalWrite(ur, HIGH); digitalWrite(lr, HIGH); delay(100); digitalWrite(ur, LOW); digitalWrite(lr, LOW);
   digitalWrite(ug, HIGH); digitalWrite(lg, HIGH); delay(100); digitalWrite(ug, LOW); digitalWrite(lg, LOW);
   digitalWrite(ub, HIGH); digitalWrite(lb, HIGH); delay(100); digitalWrite(ub, LOW); digitalWrite(lb, LOW);
   digitalWrite(ur, HIGH);
+  //測試七段顯示器
   digitalWrite(seg_rst, HIGH); digitalWrite(seg_a, LOW); digitalWrite(seg_b, LOW); digitalWrite(seg_c, LOW); digitalWrite(seg_d, LOW); delay(100); digitalWrite(seg_rst, LOW);
   digitalWrite(seg_a, HIGH); digitalWrite(seg_b, LOW); digitalWrite(seg_c, LOW); digitalWrite(seg_d, LOW); delay(100); //1
   digitalWrite(seg_a, LOW); digitalWrite(seg_b, HIGH); digitalWrite(seg_c, LOW); digitalWrite(seg_d, LOW); delay(100); //2
@@ -86,10 +96,12 @@ void setup() {
 }
 
 void loop() {
+  //loop迴圈開始，重置七段顯示器狀態
   digitalWrite(seg_rst, LOW); digitalWrite(seg_a, LOW); digitalWrite(seg_b, LOW); digitalWrite(seg_c, LOW); digitalWrite(seg_d, LOW);
 
-  //Serial.println("I2C Reading Device 1 Begin");
-  Wire.requestFrom(1, 6);
+  //開始向master要資料
+  Serial.println("I2C Requesting From Device 1 Begin");
+  Wire.requestFrom(1, 6); //向1號裝置要6byte的資料
   while (Wire.available())
   {
     val1_a = Wire.read(); val1_b = Wire.read();
@@ -101,9 +113,10 @@ void loop() {
     seg_state = val1;
     led_state_1 = val2;
     led_state_2 = val3;
-    Serial.println("I2C Reading Device 1 End");
-    Serial.println(" ");
+    Serial.println("I2C Reading From Device 1 Ended");
   }
+
+  //將要顯示的數值/顏色輸出漁serial
   Serial.print("Received 7 Segment Display Value: ");
   Serial.println(seg_state);
   Serial.print("Received LED1 Value: ");
@@ -112,8 +125,8 @@ void loop() {
   Serial.println(led_state_2);
   Serial.println("  ");
   Serial.println("  ");
-  Serial.println("  ");
 
+  //接收到的7段顯示器顯示數值轉換為輸出
   if (seg_state == 0) {
     digitalWrite(seg_rst, LOW); digitalWrite(seg_a, LOW); digitalWrite(seg_b, LOW); digitalWrite(seg_c, LOW); digitalWrite(seg_d, LOW);
     digitalWrite(seg_rst, HIGH); digitalWrite(seg_a, LOW); digitalWrite(seg_b, LOW); digitalWrite(seg_c, LOW); digitalWrite(seg_d, LOW);
@@ -160,6 +173,8 @@ void loop() {
     digitalWrite(lr, LOW); digitalWrite(lg, LOW); digitalWrite(lb, LOW);
     digitalWrite(ur, HIGH); delay(250); digitalWrite(ur, LOW);
   }
+
+  //接收到的右上系統狀態燈號轉換為輸出
   if (led_state_1 == 1) {
     digitalWrite(ur, LOW); digitalWrite(ug, LOW); digitalWrite(ub, LOW);
     digitalWrite(ur, HIGH);
@@ -173,6 +188,7 @@ void loop() {
     digitalWrite(ub, HIGH);
   }
 
+  //接收到的右下馬達狀態訊號轉換為輸出
   if (led_state_2 == 1) {
     digitalWrite(lr, LOW); digitalWrite(lg, LOW); digitalWrite(lb, LOW);
     digitalWrite(lr, HIGH);
@@ -185,7 +201,11 @@ void loop() {
     digitalWrite(lr, LOW); digitalWrite(lg, LOW); digitalWrite(lb, LOW);
     digitalWrite(lb, HIGH);
   }
+
+  //延遲 重新開始準備接收
   delay(500);
+
+  //重置數值準備重新接收訊號
   seg_state = 10;
   led_state_1 = 10;
   led_state_2 = 10;
