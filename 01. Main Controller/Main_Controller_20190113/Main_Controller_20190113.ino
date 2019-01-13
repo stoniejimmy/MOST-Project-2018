192043917cc5b91d5ced6678a0b5a336052a8ef6
stoniejimmy <stoniejimmy@users.noreply.github.com>
2019-01-13 17:26:40 +0800

    Added status led

--- 01. Main Controller/Main_Controller_20190113/Main_Controller_20190113.ino
+++ 01. Main Controller/Main_Controller_20190113/Main_Controller_20190113.ino
@@ -20,6 +20,25 @@ int seg_c = 43;
 int seg_d = 44;
 int seg_rst = 45;
 
+/*
+  狀態LED接腳說明:
+  上紅:綠線-藍線 => Pin 47
+  上綠:橘線-橘線 => Pin 48
+  上籃:白線-白線 => Pin 49
+  下紅:紅線-紅線 => Pin 50
+  下綠:黃線-棕線 => Pin 51
+  下藍:紫線-黑線 => Pin 52
+*/
+int led_state_1 = 0;
+int led_state_2 = 0;
+int ur = 47;
+int ug = 48;
+int ub = 49;
+int lr = 50;
+int lg = 51;
+int lb = 52;
+
+
 void setup() {
   Serial.begin(9600);
 
@@ -28,6 +47,13 @@ void setup() {
   pinMode(seg_c, OUTPUT);
   pinMode(seg_d, OUTPUT);
   pinMode(seg_rst, OUTPUT);
+
+  pinMode(ur, OUTPUT);
+  pinMode(ug, OUTPUT);
+  pinMode(ub, OUTPUT);
+  pinMode(lr, OUTPUT);
+  pinMode(lg, OUTPUT);
+  pinMode(lb, OUTPUT);
 
   // 開始七段顯示器測試
   digitalWrite(seg_rst, HIGH); digitalWrite(seg_a, LOW); digitalWrite(seg_b, LOW); digitalWrite(seg_c, LOW); digitalWrite(seg_d, LOW); delay(100); digitalWrite(seg_rst, LOW);
@@ -40,11 +66,18 @@ void setup() {
   digitalWrite(seg_a, HIGH); digitalWrite(seg_b, HIGH); digitalWrite(seg_c, HIGH); digitalWrite(seg_d, LOW); delay(100); //7
   digitalWrite(seg_a, LOW); digitalWrite(seg_b, LOW); digitalWrite(seg_c, LOW); digitalWrite(seg_d, HIGH); delay(100); //8
   digitalWrite(seg_a, HIGH); digitalWrite(seg_b, LOW); digitalWrite(seg_c, LOW); digitalWrite(seg_d, HIGH); delay(100); //9
+
+  digitalWrite(ur, HIGH); digitalWrite(lr, HIGH); delay(100); digitalWrite(ur, LOW); digitalWrite(lr, LOW);
+  digitalWrite(ug, HIGH); digitalWrite(lg, HIGH); delay(100); digitalWrite(ug, LOW); digitalWrite(lg, LOW);
+  digitalWrite(ub, HIGH); digitalWrite(lb, HIGH); delay(100); digitalWrite(ub, LOW); digitalWrite(lb, LOW);
+  digitalWrite(ur, HIGH);
 }
 
 void loop() {
   seg_display();
+  state_led_update();
 }
+
 void seg_display() {
   if (seg_state == 0) {
     digitalWrite(seg_rst, HIGH); digitalWrite(seg_a, LOW); digitalWrite(seg_b, LOW); digitalWrite(seg_c, LOW); digitalWrite(seg_d, LOW);
@@ -75,5 +108,32 @@ void seg_display() {
   }
   else if (seg_state == 9) {
     digitalWrite(seg_a, HIGH); digitalWrite(seg_b, LOW); digitalWrite(seg_c, LOW); digitalWrite(seg_d, HIGH);
+  }
+}
+void state_led_update() {
+  if (led_state_1 == 1) {
+    digitalWrite(ur, LOW); digitalWrite(ug, LOW); digitalWrite(ub, LOW);
+    digitalWrite(ur, HIGH);
+  }
+  else if (led_state_1 == 2) {
+    digitalWrite(ur, LOW); digitalWrite(ug, LOW); digitalWrite(ub, LOW);
+    digitalWrite(ug, HIGH);
+  }
+  else if (led_state_1 == 3) {
+    digitalWrite(ur, LOW); digitalWrite(ug, LOW); digitalWrite(ub, LOW);
+    digitalWrite(ub, HIGH);
+  }
+
+  if (led_state_2 == 1) {
+    digitalWrite(lr, LOW); digitalWrite(lg, LOW); digitalWrite(lb, LOW);
+    digitalWrite(lr, HIGH);
+  }
+  else if (led_state_2 == 2) {
+    digitalWrite(lr, LOW); digitalWrite(lg, LOW); digitalWrite(lb, LOW);
+    digitalWrite(lg, HIGH);
+  }
+  else if (led_state_2 == 3) {
+    digitalWrite(lr, LOW); digitalWrite(lg, LOW); digitalWrite(lb, LOW);
+    digitalWrite(lb, HIGH);
   }
 }
